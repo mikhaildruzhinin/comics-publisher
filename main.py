5953e81be2788060f4d796855b20ed20701644e7
Mikhail Druzhinin <druzhininmy@gmail.com>
2020-03-10 16:49:00 +0300

    Multiple fixes
    * fetch_filename_and_extension() renamed to get_filename_and_extension()
    * added additional exception raising as VK api only ever responds with code 200
    * removed exess json decoding
    * temporary files will now get deleted even if something goes wrong

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import random
 from dotenv import load_dotenv
 
 def save_pic(url, filepath):
-    filename, file_extension = fetch_filename_and_extension(url)
+    filename, file_extension = get_filename_and_extension(url)
     filename = f'{filename}{file_extension}'
     response = requests.get(url)
     response.raise_for_status()
@@ -17,7 +17,7 @@ def save_pic(url, filepath):
         file.write(response.content)
     return filename
 
-def fetch_filename_and_extension(url):
+def get_filename_and_extension(url):
     filename = url.split('/')[-1]
     filename, file_extension = os.path.splitext(filename)
     return filename, file_extension
@@ -46,8 +46,10 @@ def get_groups_info(access_token, v):
         'v': v,
     }
     response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response.json()
+    collected_data = response.json()
+    if 'error' in collected_data:
+        raise requests.exceptions.HTTPError(collected_data['error'])
+    return collected_data
 
 def get_group_upload_url(group_id, access_token, v):
     url = 'https://api.vk.com/method/photos.getWallUploadServer'
@@ -57,8 +59,10 @@ def get_group_upload_url(group_id, access_token, v):
         'v': v,
     }
     response = requests.get(url, params=payload)
-    response.raise_for_status()
-    return response.json()
+    collected_data = response.json()
+    if 'error' in collected_data:
+        raise requests.exceptions.HTTPError(collected_data['error'])
+    return collected_data
 
 def upload_pic(filename, upload_url):
     filepath = f'images/{filename}'
@@ -69,7 +73,10 @@ def upload_pic(filename, upload_url):
         }
         response = requests.post(url, files=files)
         response.raise_for_status()
-        return response.json()['photo'], response.json()['server'], response.json()['hash']
+        collected_data = response.json()
+        if 'error' in collected_data:
+            raise requests.exceptions.HTTPError(collected_data['error'])
+        return collected_data['photo'], collected_data['server'], collected_data['hash']
 
 def save_pic_in_group(group_id, photo, server, hash_, access_token, v):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
@@ -83,7 +90,10 @@ def save_pic_in_group(group_id, photo, server, hash_, access_token, v):
     }
     response = requests.post(url, params=payload)
     response.raise_for_status()
-    return response.json()['response'][0]['id'], response.json()['response'][0]['owner_id']
+    collected_data = response.json()
+    if 'error' in collected_data:
+        raise requests.exceptions.HTTPError(collected_data['error'])
+    return collected_data['response'][0]['id'], collected_data['response'][0]['owner_id']
 
 def post_pic(group_id, owner_id, media_id, message, access_token, v):
     url = 'https://api.vk.com/method/wall.post'
@@ -96,31 +106,33 @@ def post_pic(group_id, owner_id, media_id, message, access_token, v):
         'v': v,
     }
     response = requests.post(url, params=payload)
-    response.raise_for_status()
-    return response.json()
+    collected_data = response.json()
+    if 'error' in collected_data:
+        raise requests.exceptions.HTTPError(collected_data['error'])
+    return collected_data
 
 def delete_file(filename):
     filepath = f'images/{filename}'
     if os.path.isfile(filepath):
         os.remove(filepath)
 
-
 def main():
     total_number = get_comic_number()
     comic_number = random.randint(1, total_number)
     filename, message = fetch_xkcd_comic(comic_number)
-    print(message)
     
     load_dotenv()
     access_token = os.getenv('VK_ACCESS_TOKEN')
     v = 5.103
     group_id = os.getenv('VK_GROUP_ID')
 
-    upload_url = get_group_upload_url(group_id, access_token, v)['response']['upload_url']
-    photo, server, hash_ = upload_pic(filename, upload_url)
-    media_id, owner_id = save_pic_in_group(group_id, photo, server, hash_, access_token, v)
-    res = post_pic(group_id, owner_id, media_id, message, access_token, v)
-    delete_file(filename)
+    try:
+        upload_url = get_group_upload_url(group_id, access_token, v)['response']['upload_url']
+        photo, server, hash_ = upload_pic(filename, upload_url)
+        media_id, owner_id = save_pic_in_group(group_id, photo, server, hash_, access_token, v)
+        res = post_pic(group_id, owner_id, media_id, message, access_token, v)
+    finally:
+        delete_file(filename)
 
 if __name__=='__main__':
     main()
